af28a86ce44457600d66910b5eadb3a2d3f2e665
Aljosha Friemann <a.friemann@automate.wtf>
2017-07-05 20:30:08 +0200

    minor refactoring for the autocompiler module

--- atompages/autocompiler.py
+++ atompages/autocompiler.py
@@ -1,19 +1,22 @@
-import os, sys, logging
-import re
-import threading
+# coding: utf-8
+
+import os, sys, logging, re, threading
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import logging
 
-from .generate_pages import build_site
+from atompages.generate_pages import build_site
+
+logger = logging.getLogger(__name__)
 
 class FileEventHandler(FileSystemEventHandler):
     def __init__(self, updateFkt):
         self.updateFkt = updateFkt
         self.last_files = []
-        self.on_moved = self.on_created = self.on_deleted = self.on_modified = self.somethings_changed
         self.updateFkt(["."])
         self.hashes = {}
+
+        self.on_moved = self.on_created = self.on_deleted = self.on_modified = self.somethings_changed
 
     def check_has_really_changed(self, paths):
         def check_path(path):
@@ -32,28 +35,24 @@ class FileEventHandler(FileSystemEventHandler):
             if event is not None:
                 self.last_files.append(event.src_path)
                 if self.check_has_really_changed(self.last_files):
-                    logging.info("\nChanged files: \n  %s\n" % "\n  ".join(self.last_files))
+                    logger.info("\nChanged files: \n  %s\n" % "\n  ".join(self.last_files))
                     threading.Thread(target=self.updateFkt(self.last_files[:])).start()
                     self.last_files = []
         except Exception as e:
-            logging.exception(e)
+            logger.exception(e)
 
-
-logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
 
 def main(root, src_path=None, threaded=True):
-    src_path = root+"/source" if src_path is None else src_path
+    src_path = os.path.join(root, "source") if src_path is None else src_path
 
     def update_fkt(src_paths, ignore_patterns=[".*.scss.css$", ".*.scss$",".*\.pyc$", ".*\\output.*"]):
         print("Checking update for \n%s"%"\n".join(src_paths))
         for path in src_paths:
             if not True in [not not re.match(pat,path) for pat in ignore_patterns]:
-                logging.info("Building site for %s"%path)
+                logger.info("Building site for %s"%path)
                 return build_site(root)
             else:
-                logging.info("File matches ignore pattern: %s"%path)
+                logger.info("File matches ignore pattern: %s"%path)
         #if False in [True in [not not re.match(pat, path) for pat in ignore_patterns] for path in src_paths]:
     #        build_site(root)
 
@@ -61,6 +60,7 @@ def main(root, src_path=None, threaded=True):
     event_handler = FileEventHandler(update_fkt)
     observer = Observer()
     observer.schedule(event_handler, src_path, recursive=True)
+
     if threaded:
         threading.Thread(target=observer.start, args=[]).start()
     else:
@@ -70,7 +70,4 @@ def main(root, src_path=None, threaded=True):
             observer.stop()
         observer.join()
 
-
-
-if __name__ == "__main__":
-    main(base_dir+"/source")
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
